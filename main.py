03c586008c46ebb05f065db3329d2f457b424dea
Greg Ren <azznwind@yahoo.com>
2022-11-09 11:26:48 -0500

    continuous deployment

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 from flask import Flask, render_template
 #from flask_ngrok import run_with_ngrok
+from flask.wrappers import Response 
+import git
 
 app=Flask(__name__)
-#run_with_ngrok(app)
+
+@app.route('/git_update', methods=['POST'])
+def git_update():
+  repo = git.Repo('./flask-bootstrap')
+  origin = repo.remote.origin
+  repo.create_head('main', 
+  origin.refs.main).set_tracking_branch(origin.refs.main).checkout()
+  orign.pull()
+  return '', 200
 
 @app.route("/")
 def home():
