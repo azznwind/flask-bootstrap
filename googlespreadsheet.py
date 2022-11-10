98d42b2b61f0046442c5d08f351e488dcf6acf4e
Greg Ren <azznwind@yahoo.com>
2022-11-10 18:23:55 -0500

    several updates

--- googlespreadsheet.py
+++ googlespreadsheet.py
@@ -1,7 +1,18 @@
 import pandas as pd
+import numpy as np
 
 gsheetid = "1u89hD0d3ZJY73v00CipKPdcrOaPMQ1u0KUa0DUZhEQc"
 sheet_name = "journal"
 gsheet_url = "https://docs.google.com/spreadsheets/d/{}/gviz/tq?tqx=out:csv&sheet={}".format(gsheetid, sheet_name)
+
 df = pd.read_csv(gsheet_url)
+data = df.loc[:,'date entered':'buying power effect']
+#data = df.style.set_properties(**{'text-align': 'right'})
+"""
+data = df.style.format({"quoted price": "${:20,.0f}", 
+                          "curr price": "${:20,.0f}", 
+                          "max profit": "${:20,.0f}",
+                          "max loss":"${:20,.0f}"})\
+                .hide_index()
+"""
 #print(df)
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 #from flask_ngrok import run_with_ngrok
 from flask.wrappers import Response 
 import git
@@ -19,7 +19,7 @@ def git_update():
 
 @app.route("/")
 def home():
-    return render_template('index.html', tables=googlespreadsheet.df.to_html(), titles=googlespreadsheet.df.columns.values)
+    return render_template('index.html',  tables=googlespreadsheet.data.to_html(), titles=googlespreadsheet.data.columns.values)
                                 
 @app.route("/test")
 def test():
@@ -29,6 +29,20 @@ def test():
 def moment():
     
     return render_template('moment.html', name='greg')
+
+
+@app.route('/testdata', methods=['GET', 'POST'])
+def testfn():    # GET request
+    if request.method == 'GET':
+        message = googlespreadsheet.df
+        message = message.to_json()
+        #return jsonify(message)  # serialize and use JSON headers    # POST request
+        #jm = jsonify(message)
+        return render_template('testdata.html')
+    if request.method == 'POST':
+        print(request.get_json())  # parse as JSON
+        return 'Sucesss', 200
+           
 
 if __name__=="__main__":
     #app.run(host="localhost", port=5000, debug=True)
